1681313cb490b5c1d1c1d3b0bb0ecc57e6ca4d44
Paul-Georg <paul.georg.boehm@gmail.com>
2024-02-09 22:17:54 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,28 +3,29 @@
 #makeCacheMatrix consists of set, get, setinv, getinv
 
 
-makeCacheMatrix <- function(x = matrix()) {         #set the input x as a matrix
-  p <- NULL                                         #initializing p as null
+
+makeCacheMatrix <- function(x = matrix()) {          #set the input x as a matrix
+  p <- NULL                                          #initializing p as null
   set <- function(y) {
     x <<- y
     p <<- NULL
   }
-  get <- function() x                               #function to get the matrix x
-  setinverse <- function(inverse) p <<- inverse
+  get <- function() x                                #function to get the matrix x
+  setinverse <- function(inverse) p <<- inverse 
   getinverse <- function() p
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
-cacheinverse <- function(x, ...) {                  #getting cache data
+cacheinverse <- function(x, ...) {                   #getting cache data
   p <- x$getinverse()
-  if(!is.null(p)) {                                 #checking if p is null
+  if(!is.null(p)) {                                  #checking if p is null
     message("getting cached data")
-    return(p)                                       #return the p value 
+    return(p)                                        #return the p value 
   }
   matrix_to_invert <- x$get()
-  p <- solve(matrix_to_invert, ...)                 #calculates p value
+  p <- solve(matrix_to_invert, ...)                  #calculates p value
   x$setinverse(p)
-  p                                                 #p equals the inversed x matrix 
+  p                                                  #p equals the inversed x matrix 
 }
